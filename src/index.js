dd722535b8b315ad0916b5c33270366ee1039d35
gringrape <gringrape200@gmail.com>
2021-11-26 00:10:54 +0900

    Multiple notes
    Multiple notes fall.

--- src/index.js
+++ src/index.js
@@ -25,6 +25,26 @@ const downTriangle = new PIXI.Graphics()
   .endFill();
 app.stage.addChild(downTriangle);
 
+const leftTriangle = new PIXI.Graphics()
+  .beginFill(0xff00ff)
+  .lineStyle(1, 0xff00ff)
+  .moveTo(APP_SIZE.width / 2 - TRIANGLE_LENGTH / 2, TRIANGLE_LENGTH / 2)
+  .lineTo(APP_SIZE.width / 2 + TRIANGLE_LENGTH / 2, 0)
+  .lineTo(APP_SIZE.width / 2 + TRIANGLE_LENGTH / 2, TRIANGLE_LENGTH)
+  .lineTo(APP_SIZE.width / 2 - TRIANGLE_LENGTH / 2, TRIANGLE_LENGTH / 2)
+  .endFill();
+app.stage.addChild(leftTriangle);
+
+const rightTriangle = new PIXI.Graphics()
+  .beginFill(0xff00ff)
+  .lineStyle(1, 0xff00ff)
+  .moveTo(APP_SIZE.width / 2 - TRIANGLE_LENGTH / 2, 0)
+  .lineTo(APP_SIZE.width / 2 + TRIANGLE_LENGTH / 2, TRIANGLE_LENGTH / 2)
+  .lineTo(APP_SIZE.width / 2 - TRIANGLE_LENGTH / 2, TRIANGLE_LENGTH)
+  .lineTo(APP_SIZE.width / 2 - TRIANGLE_LENGTH / 2, 0)
+  .endFill();
+app.stage.addChild(rightTriangle);
+
 const LINE_POSITION_Y = APP_SIZE.height * 0.7;
 const bottomLine = new PIXI.Graphics()
   .beginFill(0x00ffff)
@@ -34,26 +54,54 @@ const bottomLine = new PIXI.Graphics()
   .endFill();
 app.stage.addChildAt(bottomLine);
 
-const miSound = sound.add('bgm', '/mi_sound.mp3');
+const miSound = sound.add('mi', '/mi_sound.mp3');
+const reSound = sound.add('re', '/re_sound.mp3');
+const doSound = sound.add('do', '/do_sound.mp3');
 
-function intersect() {
-  return LINE_POSITION_Y > downTriangle.y && LINE_POSITION_Y < downTriangle.y + TRIANGLE_LENGTH;
+function intersect(figure) {
+  return LINE_POSITION_Y > figure.y && LINE_POSITION_Y < figure.y + TRIANGLE_LENGTH;
 }
 
 window.addEventListener('keydown', (e) => {
   const { key } = e;
 
   if (key === 'ArrowDown') {
+    reSound.play();
+  }
+
+  if (key === 'ArrowLeft') {
+    doSound.play();
+  }
+
+  if (key === 'ArrowRight') {
     miSound.play();
   }
 
-  if (key === 'ArrowDown' && intersect()) {
+  if (key === 'ArrowDown' && intersect(downTriangle)) {
     app.stage.removeChild(downTriangle);
+  }
+
+  if (key === 'ArrowLeft' && intersect(leftTriangle)) {
+    app.stage.removeChild(leftTriangle);
+  }
+
+  if (key === 'ArrowRight' && intersect(rightTriangle)) {
+    app.stage.removeChild(rightTriangle);
   }
 });
 
+function initializeNotes() {
+  leftTriangle.position.y = -250;
+  downTriangle.position.y = -150;
+  rightTriangle.position.y = 0;
+}
+
+initializeNotes();
+
 function update() {
+  rightTriangle.position.y += 5;
   downTriangle.position.y += 5;
+  leftTriangle.position.y += 5;
 
   app.render(app.stage);
 
