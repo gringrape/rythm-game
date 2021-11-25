8ee9cba0eee0a6777f2b6280a385221ff35ee096
gringrape <gringrape200@gmail.com>
2021-11-25 22:19:02 +0900

    Show arrow
    Arrow suddenly falls down.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-const root = document.getElementById('root');
+import * as PIXI from 'pixi.js';
 
-root.innerHTML = `
-  <h1>Hello World</h1>
-`;
+const app = new PIXI.Application({
+  width: 640,
+  height: 360,
+  antialias: true,
+});
+
+document.getElementById('root').appendChild(app.view);
+
+const TRIANGLE_LENGTH = 60;
+
+const downTriangle = new PIXI.Graphics()
+  .beginFill(0xff00ff)
+  .lineStyle(1, 0xff00ff)
+  .lineTo(TRIANGLE_LENGTH, 0)
+  .lineTo(TRIANGLE_LENGTH / 2, TRIANGLE_LENGTH)
+  .lineTo(0, 0)
+  .endFill();
+
+app.stage.addChild(downTriangle);
+
+function update() {
+  downTriangle.position.y += 5;
+
+  app.render(app.stage);
+
+  requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
